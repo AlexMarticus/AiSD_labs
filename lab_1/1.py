75962005f7fc695e575d7c89a81bf355bf3ad921
Nadezhda-hp <144684960+Nadezhda-hp@users.noreply.github.com>
2023-09-25 23:16:29 +0300

    Update 1.py

--- lab_1/1.py
+++ lab_1/1.py
@@ -1,9 +1,10 @@
 step = 0
 left = 0
 nums = [i for i in range (1,100001)]
-task = int(input("Введите число, которое необходимо найти:"))
+task = int(input("Введите число от 1 до 100000, которое необходимо найти:"))
 right = len (nums)
 mid = 0
+
 while left + 1 < right:
     step += 1
     mid  = (left + right) // 2
